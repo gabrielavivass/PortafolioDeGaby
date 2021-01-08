1480411a3c1e37c7bbf69ff9ccfa46d5fe9843e3
Maria Gabriela Vivas <gabrielavivass13@gmail.com>
2021-01-07 21:53:02 -0300

    cambio nombre footer e iconos

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,27 @@ $(function () {
   });
 });
 
+//Typeform
+(function () {
+  var qs,
+    js,
+    q,
+    s,
+    d = document,
+    gi = d.getElementById,
+    ce = d.createElement,
+    gt = d.getElementsByTagName,
+    id = "typef_orm",
+    b = "https://embed.typeform.com/";
+  if (!gi.call(d, id)) {
+    js = ce.call(d, "script");
+    js.id = id;
+    js.src = b + "embed.js";
+    q = gt.call(d, "script")[0];
+    q.parentNode.insertBefore(js, q);
+  }
+})();
+
 //esconder parrafo de tarjetas
 $("#titulo").click(function () {
   $("#parrafo").toggle("slow");
